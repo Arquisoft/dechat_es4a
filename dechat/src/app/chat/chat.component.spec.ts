76c01ce52e617df50fd2831f38ad7c8a897e264a
albertong98 <albertng1998@gmail.com>
2019-03-08 20:36:39 +0100

    Create chat.component.spec.ts

---
+++ dechat/src/app/chat/chat.component.spec.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+import {ChatComponent} from './chat.component';
+
+describe(() => {
+	beforeEach(async(() => {
+		TestBed.configureTestModule({
+			declarations: [ChatComponent],
+		}).compileComponents();
+	}));
+	
+	beforeEach(()=>{
+		fixture = TestBed.createComponent(ChatComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+});
