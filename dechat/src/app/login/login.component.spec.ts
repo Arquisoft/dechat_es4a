b942727dad30412ba8095f213a7c03feeee27984
albertong98 <albertng1998@gmail.com>
2019-03-08 02:31:33 +0100

    Added test to check if you can login to the inrupt provider

--- dechat/src/app/login/login.component.spec.ts
+++ dechat/src/app/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import {TestBed,async} from '@angular/core/testing';
 import {LoginComponent} from './login.component';
+import { SolidProvider } from '../models/solid-provider.model';
 
 describe('LoginComponent',() => {
 	beforeEach(async(() => {
@@ -12,5 +13,16 @@ describe('LoginComponent',() => {
 		const fixture = TestBed.createComponent(LoginComponent);
 		const component = fixture.componentInstance;
 		expect(component).toBeTruthy();
+	});
+	it('should have inrupt provider',async(() => {
+		const fixture = TestBed.createComponent(LoginComponent);
+		const component = fixture.componentInstance;
+		const inruptProvider : SolidProvider {
+			name: 'Inrupt';
+			image: '/assets/images/Inrupt.png',
+			loginUrl: 'https://inrupt.net/auth',
+			desc: 'Inrupt Inc. provider'
+		}
+		expect(component.identityProviders.include(inruptProvider)).toBeTruthy;
 	});
 });
