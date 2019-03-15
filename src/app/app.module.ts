7460650360d19fbce704d89ed8597ee0fae539e9
davidcanle <davidcanle@hotmail.com>
2019-03-15 06:53:23 +0100

    Changes to merge

---
+++ src/app/app.module.ts
@@ -0,0 +1,80 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { LoginPopupComponent } from './login-popup/login-popup.component';
+import { LoginComponent } from './login/login.component';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChatComponent } from './chat/chat.component';
+
+// Services
+import { AuthService } from './services/solid.auth.service';
+import { AuthGuard } from './services/auth.guard.service';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import { TranslateModule } from '@ngx-translate/core';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'login-popup',
+    component: LoginPopupComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'card',
+    component: CardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LoginPopupComponent,
+    DashboardComponent,
+    CardComponent,
+    RegisterComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    NgSelectModule,
+    ToastrModule.forRoot(),
+    TranslateModule.forRoot(),
+    BrowserAnimationsModule //required for toastr
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
