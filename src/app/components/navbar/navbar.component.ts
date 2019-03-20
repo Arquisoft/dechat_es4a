62a3530f27e570dbffc9d66d9c3ca3be7b681f46
Aida María González <UO245925@uniovi.es>
2019-03-20 18:49:43 +0100

    Added logout button on the rigth side

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import { AuthService } from 'src/app/services/solid.auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +9,17 @@ import {Router} from "@angular/router";
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private auth: AuthService) { }
 
   ngOnInit() {
   }
 
   goToChat() {
     this.router.navigateByUrl('/chat');
+  }
+
+  logout() {
+    this.auth.solidSignOut();
   }
 
 }
