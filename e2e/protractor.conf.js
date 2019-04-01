8ebad7811e29bad9b3a4ff862a19f59f3f7f6d38
davidcanle <davidcanle@hotmail.com>
2019-04-02 00:49:21 +0200

    go to old version

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-
+const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -9,22 +9,20 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
-      "chromeOptions": {
-          "args": ["--headless", "--no-sandbox"]
-      }
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'custom',  // set to "custom" instead of cucumber.
-  frameworkPath: require.resolve('protractor-cucumber-framework'),  // path relative to the current config file
-  cucumberOpts: {
-    require: ['./e2e/features/**/*.steps.ts'], // loads step definitions
-    format: 'json: e2e-output.txt',              // enable console output (NO EN OTROS PROYECTOS EJEMPLO)
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
--- e2e/src/features/app.feature
+++
@@ -1,7 +0,0 @@
-Feature: Start the app
-
-
-  Scenario: First scenario
-    Given I open app
-    When app page loads
-    Then check "Solid App" text
