1901e01c9eff4618ad302736ab960c1fcfb8167d
davidcanle <davidcanle@hotmail.com>
2019-04-02 18:58:06 +0200

    Try to config coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -18,9 +18,13 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+        combineBrowserReports: true,
+        skipFilesWithNoCoverage: true
+
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml','coverage-istanbul'],
+
     port: 4200,
     colors: true,
     logLevel: config.LOG_INFO,
