05786423b05080aca6cc17009cf13c2c60a7da04
giramos <uo202549@uniovi.es>
2019-04-03 22:47:38 +0200

    add import

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from '../features/app.po';
+import {beforeEach, describe, it} from '@angular/core/testing/src/testing_internal';
 
 describe('workspace-project App', () => {
   let page: AppPage;
