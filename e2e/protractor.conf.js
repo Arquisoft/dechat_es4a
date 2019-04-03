6a6796bc84752c355bdc0b78f8de78f0796999c4
giramos <uo202549@uniovi.es>
2019-04-03 23:37:40 +0200

    First succesfully implemented scenario

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,25 +2,27 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/features/**/*.features'
+      './features/**/*.feature'
   ],
+
   capabilities: {
     'browserName': 'chrome'
   },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200',
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
-  cucumberOpts: {
-    require: ['./e2e/features/**/*.steps.ts'], // loads step definitions
-  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+
+    cucumberOpts: {
+        require: ['./src/steps/**/*.steps.ts'],
+    },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    
   }
 };
