8e7c899a3db32845b9cddfcb3b80b52241e55b61
ELENA\elena <elenapugapascual@hotmail.com>
2019-04-16 21:22:53 +0200

    restoring bin thus is needed for codecov

---
+++ bin/src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing'
+import {ToastrModule} from 'ngx-toastr';
+import {LoginComponent} from './login/login.component';
+import {LoginPopupComponent} from './login-popup/login-popup.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CardComponent} from './card/card.component';
+import {RegisterComponent} from './register/register.component';
+import {ChatComponent} from './chat/chat.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+        imports:[
+            RouterTestingModule, ToastrModule.forRoot()
+        ]
+    }).compileComponents();
+
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+ /* it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to solid-app!');
+  }));*/
+});
