af6b352f9abbc7e6a98f0f878704e67721ccd814
ELENA\elena <elenapugapascual@hotmail.com>
2019-04-16 21:58:42 +0200

    add feedback fo rthe user about the msg being loaded

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -42,9 +42,7 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     this.loadProfile();
     this.loadFriends();
-    this.toastr.info('The messages are being loaded, it will take just a second!');
     this.refreshMessages();
-    this.toastr.info('Thank you for your patience!');
   }
 
   loadFriends() {
@@ -255,6 +253,7 @@ export class ChatComponent implements OnInit {
 
   changeChat(name: string, photo: string) {
     //Cambiar chat cada vez que se hace click, tiene que cargar mensajes de otra persona
+    this.toastr.info('The messages are being loaded, it will take just a second!');
     this.messages = []; //vacia el array cada vez q se cambia de chat para que no aparezcan en pantalla
     this.friendActive = name;
     this.friendPhotoActive = photo;
