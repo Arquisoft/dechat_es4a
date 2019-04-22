1cffde59acdaca92b79a3c9fcee765ec1384a36f
giramos <uo202549@uniovi.es>
2019-04-22 06:07:48 +0200

    we begin: 2 scenarios (2 passed)

--- e2e/src/page/app.po.ts
+++ e2e/src/page/app.po.ts
@@ -1,11 +1,27 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
+    this.sleep(3000);
     return browser.get('/');
+  }
+
+  navigateToLogin() {
+    this.sleep(3000);
+    return browser.get('/login');
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  private sleep(milliseconds: number): void {
+    const start = new Date().getTime();
+    for (let i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
 }
--- e2e/src/steps/app.steps.ts
+++ e2e/src/steps/app.steps.ts
@@ -9,8 +9,20 @@ Before(() => {
   page = new AppPage();
 });
 
+//home_page.feature
 Given(/^I am on the home page$/, async () => {
   await page.navigateTo();
+});
+
+When(/^I do nothing \(home page\)$/, () => {});
+
+Then(/^I should see the title \(home page\)$/, async () => {
+  expect(await page.getParagraphText()).to.equal('ASW CHAT');
+});
+
+//login_page.feature
+Given(/^I am on the login page$/, async () => {
+  await page.navigateToLogin();
 });
 
 When(/^I do nothing \(login page\)$/, () => {});
