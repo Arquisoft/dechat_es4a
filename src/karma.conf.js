12a88f585e100c5e65e362d3a5824c8ca4542c3d
Aida <aidamaria.96@gmail.com>
2019-04-30 23:35:27 +0200

    Trying to fix travis again

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -32,8 +32,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-      browsers: ['Chrome'],
-      singleRun: false
-
+    browsers: ["Chrome"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
+      }
+    },
+    singleRun: false,
   });
 };
