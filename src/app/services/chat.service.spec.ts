902abea4d6e2a945bdf7c1985e639499838ad384
Aida <aidamaria.96@gmail.com>
2019-05-01 18:18:48 +0200

    Added test

--- src/app/services/chat.service.spec.ts
+++ src/app/services/chat.service.spec.ts
@@ -1,9 +1,10 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject,async } from '@angular/core/testing';
 
 import { ChatService } from './chat.service';
 import {RouterTestingModule} from "@angular/router/testing";
 import {ToastrModule} from "ngx-toastr";
 import { SolidChat } from '../models/solid-chat.model';
+import { SolidMessage } from '../models/solid-message.model';
 
 describe('ChatService', () => {
   beforeEach(() => {
@@ -34,6 +35,35 @@ describe('ChatService', () => {
     expect(service.chatuserUrl).toBe(chat1);
     expect(service.chatfriendUrl).toBe(chat2);
   }));
+
+  it('should post a message', inject([ChatService], (service: ChatService) => {
+    let date = (new Date()).toISOString();
+    let msg = new SolidMessage("user", "Hi", date);
+    service.postMessage(msg);
+  }));
+
+  it('should remove a message', inject([ChatService], (service: ChatService) => {
+    let date = (new Date()).toISOString();
+    let msg = new SolidMessage("user", "Hi", date);
+    service.removeMessage(msg);
+  }));
+
+  it('should return a message', inject([ChatService], (service: ChatService) => {
+    service.isChatCreated("https://user1.solid.community/profile/card#me","https://user2.solid.community/profile/card#me");
+  }));
+
+  it('should create base chat', inject([ChatService], (service: ChatService) => {
+    service.createBaseChat("https://user1.solid.community/private/Chat/");
+  }));
+
+  it('should reset chat',inject([ChatService], (service: ChatService) => {
+    service.resetChat();
+  }));
+
+  it('should reset chat', async(inject([ChatService],(service: ChatService) => {
+    expect(service).toBeTruthy();
+    service.loadMessages("user1","user2");
+  })));
 
 
 });
