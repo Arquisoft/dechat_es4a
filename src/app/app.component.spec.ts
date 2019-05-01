8ec046f7c5b4cfdd538e850da73a8d28eb4806dc
protojaime <33036535+protojaime@users.noreply.github.com>
2019-05-01 21:28:28 +0200

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,32 +28,7 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
     service = TestBed.get(ChatComponent);
-
-    let store = {};
-    const mockLocalStorage = {
-      getItem: (key: string): string => {
-        return key in store ? store[key] : null;
-      },
-      setItem: (key: string, value: string) => {
-        store[key] = `${value}`;
-      },
-      removeItem: (key: string) => {
-        delete store[key];
-      },
-      clear: () => {
-        store = {};
-      }
-    };
-    spyOn(localStorage, 'getItem')
-      .and.callFake(mockLocalStorage.getItem);
-    spyOn(localStorage, 'setItem')
-      .and.callFake(mockLocalStorage.setItem);
-    spyOn(localStorage, 'removeItem')
-      .and.callFake(mockLocalStorage.removeItem);
-    spyOn(localStorage, 'clear')
-      .and.callFake(mockLocalStorage.clear);
   }));
-
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -61,6 +36,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
 
   }));
+
 
   it('should create the chat', async(() => {
     inject([ChatComponent], (service: ChatComponent) => {
