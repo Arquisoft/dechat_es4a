49fe396e70d62b5dc5c2a645caa26b07d8beb4a3
ELENA\elena <elenapugapascual@hotmail.com>
2019-05-01 23:39:18 +0200

    trying changes on chrome

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -43,8 +43,8 @@ module.exports = function (config) {
   }
 
  /* if (process.env.TRAVIS) {
-    cfg.browsers = ['Chrome_travis_ci'];*/
-}
+    cfg.browsers = ['Chrome_travis_ci'];
+}*/
 
 config.set(cfg);
 };
