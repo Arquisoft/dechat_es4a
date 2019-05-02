9879cc6c11ac0ea054d2c2d75e35ae505ddd8a21
Aida <aidamaria.96@gmail.com>
2019-05-02 13:14:04 +0200

    Upgrading coverage

--- src/app/services/solid.auth.service.spec.ts
+++ src/app/services/solid.auth.service.spec.ts
@@ -94,4 +94,5 @@ describe('AuthService', () => {
     const expectedRes = {c:'elena' , c0: 'aida'};
     expect(JSON.stringify(service.getOldFriends())).toBe(JSON.stringify(expectedRes));
   });
+  
 });
