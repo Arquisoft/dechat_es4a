78129d8fc0fe6f9bc12eec4eef7f1cadedd49254
Aida <aidamaria.96@gmail.com>
2019-05-02 10:58:56 +0200

    Trying to config karma again

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -33,6 +33,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ["Chrome"],
+    customLaunchers: {
+      Chrome_travis_ci: {
+           base: 'Chrome',
+           flags: ['--no-sandbox']
+       }
+    },
     singleRun: false,
   }
 
