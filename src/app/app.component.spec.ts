c2921d06e8e700d535cc6766187f4d4854e4b982
Aida <aidamaria.96@gmail.com>
2019-05-02 14:03:58 +0200

    Revert "Revert "Revert "Merged"""
    This reverts commit 2aa715814bd148e8cae0399f53ec73dd7e9b9202.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,63 +1,50 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing'
-import { ToastrModule } from 'ngx-toastr';
-import { LoginComponent } from './login/login.component';
-import { LoginPopupComponent } from './login-popup/login-popup.component';
-import { CardComponent } from './card/card.component';
-import { RegisterComponent } from './register/register.component';
-import { ChatComponent } from './chat/chat.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
+import {LoginComponent} from './login/login.component';
+import {LoginPopupComponent} from './login-popup/login-popup.component';
+import {CardComponent} from './card/card.component';
+import {RegisterComponent} from './register/register.component';
+import {ChatComponent} from './chat/chat.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { Renderer2 } from '@angular/core';
 
 describe('AppComponent', () => {
-
-  let service: ChatComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         NavbarComponent
       ],
-      providers: [ChatComponent, Renderer2],
-      imports: [
-        RouterTestingModule, ToastrModule.forRoot()
-      ]
+        imports:[
+            RouterTestingModule, ToastrModule.forRoot()
+        ]
     }).compileComponents();
-    service = TestBed.get(ChatComponent);
-  }));
 
+  }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-
-  }));
-
-
-  it('should create the chat', async(() => {
-    inject([ChatComponent], (service: ChatComponent) => {
-      expect(service).toBeTruthy();
-    });
   }));
 
   afterEach(() => {
     TestBed.resetTestingModule();
   });
 
-
+  
   /*it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));*/
-  /* it('should render title in a h1 tag', async(() => {
-     const fixture = TestBed.createComponent(AppComponent);
-     fixture.detectChanges();
-     const compiled = fixture.debugElement.nativeElement;
-     expect(compiled.querySelector('h1').textContent).toContain('Welcome to solid-app!');
-   }));*/
+ /* it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to solid-app!');
+  }));*/
 });
